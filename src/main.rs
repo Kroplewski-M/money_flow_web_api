7336184a4c74443afdc89b470acf28a6a8d7d9d9
Mateusz Kroplewski <kroplewskimateusz@gmail.com>
2025-12-10 18:47:54 +0000

    docker + rate limiting

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use crate::controllers::{auth, categories, me, transactions};
 use crate::models::shared::AppState;
+use actix_extensible_rate_limit::RateLimiter;
+use actix_extensible_rate_limit::backend::SimpleInputFunctionBuilder;
+use actix_extensible_rate_limit::backend::memory::InMemoryBackend;
 use actix_web::{App, HttpServer, web};
 use dotenv::dotenv;
 use std::env;
+use std::time::Duration;
 mod controllers;
 mod data;
 mod middleware;
@@ -22,8 +26,21 @@ async fn main() -> std::io::Result<()> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::DEBUG)
         .init();
+
+    let rate_limiter_backend = InMemoryBackend::builder().build();
+
     HttpServer::new(move || {
         App::new()
+            .wrap(
+                RateLimiter::builder(
+                    rate_limiter_backend.clone(),
+                    SimpleInputFunctionBuilder::new(Duration::from_secs(60), 50)
+                        .real_ip_key()
+                        .build(),
+                )
+                .add_headers()
+                .build(),
+            )
             .app_data(state.clone())
             .configure(auth::configure)
             .configure(me::configure)
